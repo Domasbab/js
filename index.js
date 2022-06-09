5fe4e172a6ab4c3a0bf8043a07d8e5257b99b427
Domasbab <105633782+Domasbab@users.noreply.github.com>
2022-06-09 16:17:34 +0300

    lsas

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 const username = 'petras';
 
 let pinigine = 0;
-console.log(username, 'pinigine turi' pinigine, 'pinigu');
+console.log(username, 'pinigine turi', pinigine, 'pinigu');
 
-pinigine = 0;
-console.log(username, 'pinigine turi' pinigine, 'pinigu');
+pinigine = pinigine + 1;
+console.log(username, 'pinigine turi', pinigine, 'pinigu');
 
-pinigine = 0;
-console.log(username, 'pinigine turi' pinigine, 'pinigu');
+pinigine = pinigine + 1;
+console.log(username, 'pinigine turi', pinigine, 'pinigu');
+
+pinigine = pinigine + 1;
+console.log(username, 'pinigine turi', pinigine, 'pinigu');
+
+
+const a = 7;
+const b = 5;
+console.log(a,'+',b, '=', a + b);
+
+console.log(a,'-',b, '=', a - b);
+console.log(a,'/',b, '=', a / b);
+console.log(a,'*',b, '=', a * b);
+console.log(a,'-',b, '=', a + b);
