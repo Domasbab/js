bb81de0666efa43d4a2e634f78707b18e376446e
Domasbab <105633782+Domasbab@users.noreply.github.com>
2022-06-09 15:00:50 +0300

    pradedam

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const username = 'petras';
+
+let pinigine = 0;
+console.log(username, 'pinigine turi' pinigine, 'pinigu');
+
+pinigine = 0;
+console.log(username, 'pinigine turi' pinigine, 'pinigu');
+
+pinigine = 0;
+console.log(username, 'pinigine turi' pinigine, 'pinigu');
